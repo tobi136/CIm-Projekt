20604e4e59bce27b577f570ed2485f99f03dbc60
jmueller95 <julian.mueller@student.uni-tuebingen.de>
2017-05-25 10:37:41 +0200

    Roc-Point-Generator verbessert, damit man mit R 'ne Kurve plotten kann

--- main/example_predictor.py
+++ main/example_predictor.py
@@ -6,11 +6,14 @@ from random import random
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Random Epitope Binding Predictor")
-    parser.add_argument('input', metavar='in-file')
-    parser.add_argument('output', metavar='out-file')
+
+    parser.add_argument('input', metavar='input file')
+    parser.add_argument('output', metavar='output file')
+
     args = parser.parse_args()
 
     with open(args.output, "w") as o:
         with open(args.input, "r") as i:
             for e in i:
                 o.write("%s\t%i\n" % (e.strip(), random() >= 0.5))
+
--- roc/roc_point_generator.py
+++ roc/roc_point_generator.py
@@ -1,8 +1,12 @@
 from math import inf
 
 
-def generate_roc(l, f, p, n):
-    #Liste muss hier schon sortiert sein!
+def generate_roc(example_set, p, n):
+    # Absteigend Sortieren
+    example_set = list(reversed(sorted(example_set)))
+    # print(example_set)
+    f = [x for (x, y) in example_set]  # 1. Element aus jedem Tupel rausholen
+    l = [y for (x, y) in example_set]  # 2. Element...
     fp = tp = 0
     r = []
     f_prev = -inf
@@ -20,13 +24,10 @@ def generate_roc(l, f, p, n):
     return r
 
 
-# Test stuff
+# Werte aus der Aufgabe
 test_set = [(0.84, True), (0.71, True), (0.92, True), (0.39, True), (0.44, True), (0.79, False), (0.28, False),
             (0.11, False), (0.52, False), (0.31, False), (0.49, False), (0.42, False)]
-#Hier wird sortiert, und zwar absteigend nach der ersten Komponente
-#TODO: Das erst in der Methode machen
-test_set = list(reversed(sorted(test_set)))
-examples = [y for (x, y) in test_set]
-scores = [x for (x, y) in test_set]
-roc_points = generate_roc(examples, scores, 5, 7)
-print(roc_points)
+roc_points = generate_roc(test_set, 5, 7)
+# Schön formatieren, damit man's in R reinkopieren kann zum Plotten
+print("(" + ",".join(map(str, [x for (x, y) in roc_points])) + ")")  # x-Werte
+print("(" + ",".join(map(str, [y for (x, y) in roc_points])) + ")")  # y-Werte
