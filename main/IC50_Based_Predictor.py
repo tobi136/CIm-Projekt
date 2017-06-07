28c045d016b98f91771eaeaba8c36b4bbc4c9271
jmueller95 <julian.mueller@student.uni-tuebingen.de>
2017-06-07 17:02:08 +0200

    Occurence-Matrizen berechnet; 2 und 9 scheinen Ankerpositionen zu sein

--- main/IC50_Based_Predictor.py
+++ main/IC50_Based_Predictor.py
@@ -8,26 +8,22 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Random Epitope Binding Predictor")
 
     parser.add_argument('input', metavar='input file')
-    parser.add_argument('output', metavar='output file')
 
     args = parser.parse_args()
 
     binderICs = []
     nonBinderICs = []
-
-    with open(args.output, "w") as o:
-        with open(args.input, "r") as i:
-            for e in i:
-                lineSplit = e.split()
-                if lineSplit[0] == "Peptide":
-                    pass
+    with open(args.input, "r") as i:
+        for e in i:
+            lineSplit = e.split()
+            if lineSplit[0] == "Peptide":
+                pass
+            else:
+                if int(lineSplit[2]) == 1:
+                    binderICs.append(float(lineSplit[1]))
                 else:
-                    if int(lineSplit[2]) == 1:
-                        binderICs.append(float(lineSplit[1]))
-                    else:
-                        nonBinderICs.append(float(lineSplit[1]))
-            maxBinderIC = max(binderICs)
-            minNonBinderIC = min(nonBinderICs)
-            print(nonBinderICs)
-            print("Maximaler IC für Binder: " + str(maxBinderIC) + ", Minimaler IC für Nichtbinder: " + str(minNonBinderIC))
-
+                    nonBinderICs.append(float(lineSplit[1]))
+        maxBinderIC = max(binderICs)
+        minNonBinderIC = min(nonBinderICs)
+        print(nonBinderICs)
+        print("Maximaler IC für Binder: " + str(maxBinderIC) + ", Minimaler IC für Nichtbinder: " + str(minNonBinderIC))
---
+++ main/Occurence_Matrix.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+import argparse
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Random Epitope Binding Predictor")
+
+    parser.add_argument('input', metavar='input file')
+
+    args = parser.parse_args()
+
+    # Parse the sequences into two String lists of binder and non-binder sequences
+    binders = []
+    nonBinders = []
+    with open(args.input, "r") as i:
+        for e in i:
+            lineSplit = e.split()
+            if lineSplit[0] == "Peptide":
+                pass
+            else:
+                if int(lineSplit[2]) == 1:
+                    binders.append(lineSplit[0])
+                else:
+                    nonBinders.append(lineSplit[0])
+    # Count the occurences at each position
+    # First, create two dictionaries with one-letter-codes as keys and integer lists of length 9 as values
+    binderOccurenceDict = {}
+    nonBinderOccurenceDict = {}
+    oneLetterCodes = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W',
+                      'Y']
+    for letter in oneLetterCodes:
+        binderOccurenceDict[letter] = [0] * 9
+        nonBinderOccurenceDict[letter] = [0] * 9
+
+    # Now iterate over the two sequence lists and count the occurrences of every AA at every position
+    for seq in binders:
+        index = 0
+        for residue in seq:
+            binderOccurenceDict[residue][index] += 1
+            index += 1
+
+    for seq in nonBinders:
+        index = 0
+        for residue in seq:
+            nonBinderOccurenceDict[residue][index] += 1
+            index += 1
+
+    #Print the dictionaries as tables
+    print("BINDER OCCURRENCE MATRIX")
+    for key,value in sorted(binderOccurenceDict.items()):
+        print(key + ": " + str(value))
+
+    print("-----------------------------")
+    print("NON-BINDER OCCURRENCE MATRIX")
+    for key, value in sorted(nonBinderOccurenceDict.items()):
+        print(key + ": " + str(value))
